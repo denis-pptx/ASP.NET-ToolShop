afa11686b577b1c74e1d05cc636f9aa2493971ad
Denis-HP <denis.pptx@gmail.com>
2023-09-18 09:40:34 +0300

    lab1

---
+++ WEB_153503_Konchik/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153503_Konchik.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ WEB_153503_Konchik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_153503_Konchik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
