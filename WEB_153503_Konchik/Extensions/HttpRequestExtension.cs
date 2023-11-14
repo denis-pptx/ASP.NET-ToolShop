06fa70aafa7f1ff374c487255af7f6416ddb32fa
Denis-PC <denis.pptx@gmail.com>
2023-11-14 20:28:21 +0300

    HttpRequest extension added

---
+++ WEB_153503_Konchik/Extensions/HttpRequestExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_153503_Konchik.Extensions;
+
+public static class HttpRequestExtension
+{
+    public static bool IsAjaxRequest(this HttpRequest httpRequest)
+    {
+        return httpRequest.Headers["x-requested-with"].ToString().ToLower().Equals("xmlhttprequest");
+    }
+}
