0007d530f5885548312e97dae18800615975fafa
Denis-PC <denis.pptx@gmail.com>
2023-11-15 21:46:02 +0300

    cart models created

---
+++ WEB_153503_Konchik.Domain/Models/Cart.cs
@@ -0,0 +1,57 @@
+﻿using WEB_153503_Konchik.Domain.Entities;
+
+namespace WEB_153503_Konchik.Domain.Models;
+
+public class Cart
+{
+    /// <summary>
+    /// Список объектов в корзине
+    /// key - идентификатор объекта
+    /// </summary>
+    public Dictionary<int, CartItem> CartItems { get; set; } = new();
+
+    /// <summary>
+    /// Добавить объект в корзину
+    /// </summary>
+    /// <param name="dish">Добавляемый объект</param>
+    public virtual void AddToCart(Tool tool)
+    {
+        if (CartItems.ContainsKey(tool.Id))
+            CartItems[tool.Id].Quantity++;
+        else
+            CartItems[tool.Id] = new CartItem()
+            {
+                Tool = tool,
+                Quantity = 1
+            };
+    }
+
+    /// <summary>
+    /// Удалить объект из корзины
+    /// </summary>
+    /// <param name="id"> id удаляемого объекта</param>
+    public virtual void RemoveItems(int id)
+    {
+        CartItems.Remove(id);
+    }
+
+    /// <summary>
+    /// Очистить корзину
+    /// </summary>
+    public virtual void ClearAll()
+    {
+        CartItems.Clear();
+    }
+
+    /// <summary>
+    /// Количество объектов в корзине
+    /// </summary>
+    public int Count => 
+        CartItems.Sum(item => item.Value.Quantity);
+
+    /// <summary>
+    /// Общее сумма корзины
+    /// </summary>
+    public double TotalPrice => 
+        CartItems.Sum(item => item.Value.Tool.Price * item.Value.Quantity);
+}
---
+++ WEB_153503_Konchik.Domain/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿using WEB_153503_Konchik.Domain.Entities;
+
+namespace WEB_153503_Konchik.Domain.Models;
+
+public class CartItem
+{
+    public Tool Tool { get; set; } = null!;
+    public int Quantity { get; set; }
+}
