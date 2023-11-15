520ee96a9f24dcf0dcbf1132de45e33df28bf226
Denis-PC <denis.pptx@gmail.com>
2023-11-15 22:33:54 +0300

    implemented sessions for cart

--- WEB_153503_Konchik/Components/CartViewComponent.cs
+++ WEB_153503_Konchik/Components/CartViewComponent.cs
@@ -1,9 +1,17 @@
-﻿namespace WEB_153503_Konchik.Components;
+﻿using WEB_153503_Konchik.Extensions;
+
+namespace WEB_153503_Konchik.Components;
 
 public class CartViewComponent : ViewComponent
 {
+    private readonly IHttpContextAccessor _contextAccessor;
+    public CartViewComponent(IHttpContextAccessor httpContextAccessor)
+    {
+        _contextAccessor = httpContextAccessor;
+    }
     public IViewComponentResult Invoke()
     {
-        return View();
+        Cart? cart = _contextAccessor.HttpContext!.Session.Get<Cart>(nameof(Cart));
+        return View(cart);
     }
 }
---
+++ WEB_153503_Konchik/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using WEB_153503_Konchik.Extensions;
+
+namespace WEB_153503_Konchik.Controllers;
+
+[Authorize]
+public class CartController : Controller
+{
+    private readonly IToolService _toolService;
+    public CartController(IToolService toolService)
+    {
+        _toolService = toolService;
+    }
+
+    public async Task<IActionResult> Add(int id, string returnUrl)
+    {
+        var tool = await _toolService.GetToolByIdAsync(id);
+        if (tool.Success)
+        {
+            Cart cart = HttpContext.Session.Get<Cart>(nameof(Cart)) ?? new();
+            cart.AddToCart(tool.Data!);
+
+            HttpContext.Session.Set(nameof(Cart), cart);
+        }
+
+        return Redirect(returnUrl);
+    }
+}
